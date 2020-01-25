d2c6d1c59b9208218ccf3a81b4057cded62bed96
Owen Hellum <omch20012@gmail.com>
2020-01-25 14:47:36 -0500

    Completed the basic parts of how to create and set the tone

--- soundScript.js
+++ soundScript.js
@@ -1,17 +1,26 @@
 // TESTING ZONE
 
-//create a synth and connect it to the master output (your speakers)
-var synth = new Tone.Synth().toMaster();
-
-//The pitch of the tone to be played
+//The current instrument selected
+var sound = new Tone.Synth().toMaster();
+//The pitch of the tone to be played (A through G)
 var tonality = "C";
-//The length of the tone
+//The register of the tonality (1 to 6)
+var register = "4";
+//The length of the tone (Whole note to thirty-second note)
 var sustain = "4";
-//The loudness of the tone
-var dynamics = 0;
+//The loudness of the tone (-20 to 20)
+var dynamics = 5;
+
+function setInst(instrumentType) {
+  sound = instrumentType;
+}
 
 function setTone(toneLetter) {
   tonality = toneLetter;
+}
+
+function setRegi(registerNumber) {
+  register = registerNumber;
 }
 
 function setSust(noteType) {
@@ -20,13 +29,11 @@ function setSust(noteType) {
 
 function setDyna(decibel) {
   dynamics = decibel;
+  sound.volume.value = dynamics;
 }
 
 function playTone() {
-  //play a middle 'C' for the duration of an 8th note
-  synth.triggerAttackRelease(tonality + "4", sustain + "n");
+  sound.triggerAttackRelease(tonality + register, sustain + "n");
 }
-
-//TypeError: Argument 1 of AudioParam.setValueAtTime is not a finite floating-point value.
 
 //
